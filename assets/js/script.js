2fc4444f06a7731db2912c4d684cebcda56e15f2
subwayaintfresh <79260476+subwayaintfresh@users.noreply.github.com>
2021-04-12 13:46:42 +1000

    adding navbar, jumbo and search aside

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// uses open weather API to retrieve weather data
+// uses local storage to store persistent data 
+
+// dom elements to address areas where things should be added 
+// that day weather forcast
+// 5 cards with forcast
+// local storage buttons with previous search 
+
+// function to display jumbo
